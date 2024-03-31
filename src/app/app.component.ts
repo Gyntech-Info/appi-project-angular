5d05744dac1d7f1d095b21e442a40487e7fcf66a
gleysonareas <gleysonareasdasilva@hotmail.com>
2024-03-30 21:26:56 -0300

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { FooterComponent } from "./core/template/footer/footer.component";
+import { HeaderComponent } from "./core/template/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'appi-project-angular';
+
+  // isNgIf = false
+  isIf = false
+  isElseIf = false
+
 }
