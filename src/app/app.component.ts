c51ea40657de1eb5edf69359cc7d20483f7bb548
Gleyson Areas Da Silva <gleysonareasdasilva@gmail.com>
2024-04-02 00:42:15 -0300

    update project create components and implements routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from "@angular/common";
-import { FooterComponent } from "./core/template/footer/footer.component";
-import { HeaderComponent } from "./core/template/header/header.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './core/template/footer/footer.component';
+import { HeaderComponent } from './core/template/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
-  // isNgIf = false
-  isIf = false
-  isElseIf = false
-
+  public menu = [
+    { name: 'Home', path: '' },
+    { name: 'Modulos', path: 'modules' },
+    { name: 'Crud', path: 'crud' },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ModulesComponent } from './pages/modules/modules.component';
+import { CrudComponent } from './pages/crud/crud.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'modules',
+    component: ModulesComponent,
+  },
+  {
+    path: 'crud',
+    component: CrudComponent,
+  },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
