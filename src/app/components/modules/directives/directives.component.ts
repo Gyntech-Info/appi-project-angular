21fddf701be4252d6343cea198417c281e7e1725
gleysonareas <gleysonareasdasilva@hotmail.com>
2024-04-23 22:57:09 -0300

    update project

--- src/app/components/modules/directives/directives.component.ts
+++ src/app/components/modules/directives/directives.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IPropertiesData } from "../../../shared/models/properties-data.interface";
 import { PropertiesDataConst } from "../../../shared/utils/consts/properties-data.const";
 import { CardsComponent } from "../../../shared/components/cards/cards.component";
@@ -13,18 +13,13 @@ import { CommonModule } from "@angular/common";
 })
 export class DirectivesComponent {
 
+  @Input()
+  public isVisible: boolean = false;
+
   public selectedTemplate!: string;
-  public isVisible: boolean = true;
-  public buttonName: string = "Mostrar";
   public directivesData: IPropertiesData[] = PropertiesDataConst;
 
   loadTemplate(data: any) {
     this.selectedTemplate = data;
   }
-
-  // setStatus() {
-  //   this.isVisible = !this.isVisible;
-  //   this.buttonName = this.isVisible ? 'Esconder' : 'Mostrar';
-  // }
-
 }
