dac2bc8fdf091a8f9cee89bacb567f61524eab41
gleysonareas <gleysonareasdasilva@hotmail.com>
2024-09-13 14:42:05 -0300

    update project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withHashLocation, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes,
+      withPreloading(PreloadAllModules),
+      withHashLocation()
+    ),
+    provideAnimationsAsync()
+  ]
 };
